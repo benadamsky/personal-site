fb105c5262c440edf906411d8b231776101a67ab
Ben Adamsky <benadamsky@gmail.com>
2023-02-15 17:35:44 -0500

    used to working with nextseo oops or whatever

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,16 +6,6 @@ const Document = () => (
       <link rel="icon" href="/favicon.ico" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="title" content="Ben Adamsky - Building stuff" />
-      <meta
-        name="description"
-        content="Founder, developer, UI/UX designer. Always looking to connect with interesting people because we can't build an exciting (and sustainable) future alone."
-      />
-      <meta property="og:title" content="Ben Adamsky - Building stuff" />
-      <meta
-        property="og:description"
-        content="Founder, developer, UI/UX designer. Always looking to connect with interesting people because we can't build an exciting (and sustainable) future alone."
-      />
       <meta property="og:image" content="https://benadamsky.com/favicon.png" />
     </Head>
     <body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,16 @@ const Home = () => (
   <>
     <Head>
       <title>Ben Adamsky - Building stuff</title>
+      <meta name="title" content="Ben Adamsky - Building stuff" />
+      <meta
+        name="description"
+        content="Founder, developer, UI/UX designer. Always looking to connect with interesting people because we can't build an exciting (and sustainable) future alone."
+      />
+      <meta property="og:title" content="Ben Adamsky - Building stuff" />
+      <meta
+        property="og:description"
+        content="Founder, developer, UI/UX designer. Always looking to connect with interesting people because we can't build an exciting (and sustainable) future alone."
+      />
     </Head>
     <div className="h-full relative bg-ben-black flex flex-col items-center justify-center">
       <h1 className="font-sans md:text-8xl text-5xl text-white mb-2">
